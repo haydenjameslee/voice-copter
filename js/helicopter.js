74d0d9f844812c998a7fd0eeb9b0ea104afa483e
Daniel Golman <dgolman@vt.edu>
2014-01-18 11:48:59 -0500

    LEADERBOARDS

--- js/helicopter.js
+++ js/helicopter.js
@@ -19,7 +19,7 @@ var KEY = {
 var context;
 var analyser;
 var backgroundNoise;
-
+var leaderboard = new Clay.Leaderboard( { id: 2638 } );
 var canvas;
 
 (function () {
@@ -69,11 +69,31 @@ Heli.User = function (params) {
   function finished() {
     if (_distance > bestDistance()) {
       localStorage.bestDistance = _distance;
-      var leaderboard = new Clay.Leaderboard( { id: 2638 } );
-      leaderboard.post( { score: 500 }, function( response ) {
+      var person = prompt("Please enter your name","");
+      var options = {
+          score: _distance,
+          name: person
+      }
+      var show_options = { // all of these are optional
+          html: "<strong>Hi</strong>", // Optional, any custom html you want to show below the name
+          recent: 3600, // Optional, to limit scores to ones posted in last x seconds
+          sort: 'asc', // Optional, sorting by "asc" will show the lowest scores first (ex. for fastest times)
+          filter: ['day', 'month'], // Optional, Array of filters to narrow down high scores
+          cumulative: false, // Optional, if set to true grabs the sum of all scores for each player
+          best: false, // Optional, if set to true grabs the best score from each player
+          limit: 10, // Optional, how many scores to show (0 for all). Default is 10
+          self: false, // Optional, Boolean if set to true shows just the scores of the player viewing
+          friends: false, // Optional, Boolean if set to true shows just the scores of the player viewing AND their Clay.io friends
+          showPersonal: true // Optional, Boolean on if the player's stats (rank & high score) should show below the name. Default is false
+      };
+      
+      leaderboard.post( options, function( response ) {
           // Callback
+        leaderboard.show( show_options, function( response ) { // Optional
           console.log( response );
-      } );
+        });
+          console.log( response );
+      });
     }
   }
 
